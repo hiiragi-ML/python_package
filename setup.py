9ae9d6bd9f69d2ffe02fa1f18c23b8026b647a11
hiiragi-ML <shuseiito@gmail.com>
2022-01-06 00:56:31 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="samplelib",
+    version="1.0",
+    description="Pythonディレクトリ構成のテスト用",
+    author="hiiragi-ML",
+    author_email="hiiragisatoru1221@gmail.com",
+    url=
+)
